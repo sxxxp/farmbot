0f4890b21391e045ffd0742a88ac90464fd901ec
sxxxp <junhyeonsin@gmail.com>
2026-08-06 17:28:17 +0900

    add errorcheck custom

--- utils/errorcheck.py
+++ utils/errorcheck.py
@@ -7,7 +7,12 @@ from service.user import UserService
 class UserNotRegistered(app_commands.AppCommandError):
     """유저가 DB에 없을 때 발생시킬 커스텀 예외"""
 
-    pass
+    def __init__(
+        self,
+        message: str = "가입되지 않은 유저입니다. `/회원가입`을 먼저 진행해 주세요.",
+    ):
+        self.message = message
+        super().__init__(self.message)
 
 
 async def is_registered_user(interaction: discord.Interaction) -> bool:
@@ -15,9 +20,6 @@ async def is_registered_user(interaction: discord.Interaction) -> bool:
     user = await UserService(db).get_user(interaction.user.id)
 
     if not user:
-        # DB에 없다면 예외를 발생시킴
-        raise UserNotRegistered(
-            "가입되지 않은 유저입니다. `/회원가입`을 먼저 진행해 주세요."
-        )
+        raise UserNotRegistered()
 
     return True
